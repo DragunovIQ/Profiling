9569a9d58c772179987c1b13637dde70c6ac8a06
DragunovIQ <smoke.hummer@hotmail.com>
2021-01-02 18:00:03 +0300

    newUpdate

--- login.js
+++ login.js
@@ -17,7 +17,7 @@ console.log("Hello");
           },
           // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
           signInFlow: 'popup',
-          signInSuccessUrl: 'index/index.html',
+          signInSuccessUrl: 'index/home.html',
           signInOptions: [
             // Leave the lines as is for the providers you want to offer your users.
           //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -28,7 +28,7 @@ console.log("Hello");
           //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
           ],
           // Terms of service url.
-          tosUrl: 'index/index.html',
+          tosUrl: 'index/home.html',
           // Privacy policy url.
           privacyPolicyUrl: '<your-privacy-policy-url>'
      };
