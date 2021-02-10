c8f4f3d3aecb1772c0deab53f90db955d7d4a2a4
Dragunov <smoke.hummer@hotmail.com>
2021-02-10 14:33:22 +0300

    Update

--- home.js
+++ home.js
@@ -2,6 +2,60 @@ var mainApp = {};
 (function(){
      var firebase = app_fireBase;
      var uid = null;
+     var s;
+     var latitude;
+     var longitude;
+
+     function success(data){
+  
+          var apikey = '45db3c31cfae4d4e90c23d301d447a61';
+          latitude = data.coords.latitude;
+          longitude = data.coords.longitude;
+     
+          var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+     
+          var request_url = api_url
+            + '?'
+            + 'key=' + apikey
+            + '&q=' + encodeURIComponent(latitude + ',' + longitude)
+            + '&pretty=1'
+            + '&no_annotations=1';
+     
+          // see full list of required and optional parameters:
+          // https://opencagedata.com/api#forward
+     
+          var request = new XMLHttpRequest();
+          request.open('GET', request_url, true);
+     
+          request.onload = function() {
+            // see full list of possible response codes:
+            // https://opencagedata.com/api#codes
+     
+            if (request.status === 200){ 
+              // Success!
+              var data = JSON.parse(request.responseText);
+              //alert(data.results[0].formatted); // print the location
+              s = data.results[0].formatted;
+            } else if (request.status <= 500){ 
+              // We reached our target server, but it returned an error
+                                   
+              console.log("unable to geocode! Response code: " + request.status);
+              var data = JSON.parse(request.responseText);
+              console.log('error msg: ' + data.status.message);
+            } else {
+              console.log("server error");
+            }
+          };
+     
+          request.onerror = function() {
+            // There was a connection error of some sort
+            console.log("unable to connect to server");        
+          };
+          request.send();  // make the request
+     
+     }
+     navigator.geolocation.getCurrentPosition(success,console.error)
+     
      firebase.auth().onAuthStateChanged(function(user) {
           if (user) {
                var market;
@@ -32,8 +86,8 @@ var mainApp = {};
                }
                
                var sub = fetching("sub");
-               var messageRef = firebase.database().ref('messages/users');
-               function saveMessage(name,lname,phone,info,date,marketer){
+               var messageRef = firebase.database().ref('messages/marketers');
+               function saveMessage(name,lname,phone,info,date,marketer,loc,lat,long){
                     var newMessageRef = messageRef.push();
                     newMessageRef.set({
                          Name: name,
@@ -41,7 +95,11 @@ var mainApp = {};
                          Phone: phone,
                          Info: info,
                          Date: date,
-                         Marketer: marketer
+                         Marketer: marketer,
+                         Location: loc,
+                         Longitude: long,
+                         Latitude: lat,
+                     
                     });
                }
                
@@ -65,7 +123,7 @@ var mainApp = {};
                          + currentdate.getHours() + ":"  
                          + currentdate.getMinutes() + ":" 
                          + currentdate.getSeconds();
-                    saveMessage(name,lname,pnum,region,datetime,market);
+                    saveMessage(name,lname,pnum,region,datetime,market,s,latitude,longitude);
                     fetch("main_form").reset();
                     console.log("Data sent");
                     $(".alert").slideDown();
